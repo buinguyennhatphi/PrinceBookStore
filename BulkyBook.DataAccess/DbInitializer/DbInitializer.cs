c80d18d70de2e21ea72a1fa191a44eb0a7fb9994
Bui Nguyen Nhat Phi <riotter2703@gmail.com>
2024-11-25 17:52:21 +0700

    Add project files.

---
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,77 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db
+            )
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            //migrations if they are not applied.
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            //create roles if they are not created
+
+            if (!_roleManager.RoleExistsAsync(StaticDetails.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_User_Indi)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_User_Comp)).GetAwaiter().GetResult();
+
+                //if roles are not created, then we will create admin user as well.
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admintest@dotnet.com",
+                    Email = "riotter2703@gmail.com",
+                    Name = "Bùi Nguyễn Nhật Phi",
+                    PhoneNumber = "0303000333",
+                    StreetAddress = "90 TD, Phường 4, Quận 8",
+                    State = "Hồ Chí Minh",
+                    PostalCode = "700000",
+                    City = "Hồ Chí Minh"
+                }, "Admin@123").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "riotter2703@gmail.com");
+
+                _userManager.AddToRoleAsync(user, StaticDetails.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
+        }
+    }
+}
